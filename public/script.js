7d9a759ae8921f3d58cbc76bbf797c97e2a0a73c
--X <106478606+xcode9xx@users.noreply.github.com>
2024-05-30 18:45:51 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,79 +1,76 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const textInput = document.getElementById('text-input');
-const fontSizeInput = document.getElementById('fontSize');
-const textColorInput = document.getElementById('text-color');
-const fontFamilyInput = document.getElementById('font-family');
-let fontSize = parseInt(fontSizeInput.value);
-const image = new Image();
-image.src = './img/sign.webp';
-const downloadButton = document.querySelector('button');
+window.addEventListener('DOMContentLoaded', (event) => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const textInput = document.getElementById('text-input');
+  const fontSizeInput = document.getElementById('font-size');
+  const textColorInput = document.getElementById('text-color');
+  const fontFamilyInput = document.getElementById('font-family');
 
-downloadButton.addEventListener('click', preventZoomAndScroll);
-textInput.addEventListener('click', preventZoomAndScroll);
-textColorInput.addEventListener('click', preventZoomAndScroll);
-fontFamilyInput.addEventListener('click', preventZoomAndScroll);
-fontSizeInput.addEventListener('click', preventZoomAndScroll);
+  let fontSize = parseInt(fontSizeInput.value);
 
-image.onload = () => {
-  initializeCanvas();
-  drawImageWithText();
-};
+  const image = new Image();
+  image.src = './img/sign.webp';
 
-textInput.addEventListener('input', drawImageWithText);
-fontSizeInput.addEventListener('input', () => {
-  fontSize = parseInt(fontSizeInput.value);
-  drawImageWithText();
-});
-textColorInput.addEventListener('input', drawImageWithText);
-fontFamilyInput.addEventListener('change', drawImageWithText);
+  const downloadButton = document.querySelector('button');
+  downloadButton.addEventListener('click', preventZoomAndScroll);
 
-function initializeCanvas() {
-  canvas.width = image.width;
-  canvas.height = image.height;
-}
-
-function drawImageWithText() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(image, 0, 0);
-  const fontFamily = document.getElementById('font-family').value;
-  ctx.font = `${fontSize}px ${fontFamily}`;
-  ctx.fillStyle = document.getElementById('text-color').value;
-  ctx.textAlign = 'center';
-  ctx.fillText(textInput.value, canvas.width / 2, canvas.height / 1.8);
-  updateCurrentFontSize();
-}
-
-function downloadImage() {
-  const randomCode = Math.floor(Math.random() * 100).toString().padStart(2, '0');
-  canvas.toBlob((blob) => {
-    saveAs(blob, `edited_image_${randomCode}.png`);
+  textInput.addEventListener('input', drawImageWithText);
+  fontSizeInput.addEventListener('input', () => {
+    fontSize = parseInt(fontSizeInput.value);
+    drawImageWithText();
   });
-}
+  textColorInput.addEventListener('input', drawImageWithText);
+  fontFamilyInput.addEventListener('change', drawImageWithText);
 
-function isStandaloneBrowser() {
-  const isStandalone = window.navigator.standalone || window.matchMedia('(display-mode: standalone)').matches;
-  return !isStandalone;
-}
+  image.onload = () => {
+    initializeCanvas();
+    drawImageWithText();
+  };
 
-function updateCurrentFontSize() {
-  const currentFontSizeElement = document.getElementById('current-font-size');
-  currentFontSizeElement.textContent = fontSize;
-}
-
-function hideOnDesktop() {
-  const browserWarning = document.getElementById('browser-warning');
-  const isDesktop = window.innerWidth >= 768;
-  if (isDesktop) {
-    browserWarning.style.display = 'none';
-  } else {
-    browserWarning.style.display = 'block';
+  function initializeCanvas() {
+    canvas.width = image.width;
+    canvas.height = image.height;
   }
-}
 
-function preventZoomAndScroll(event) {
-  event.preventDefault();
-}
+  function drawImageWithText() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, 0, 0);
 
-window.addEventListener('resize', hideOnDesktop);
-window.addEventListener('load', hideOnDesktop);
+    const fontFamily = document.getElementById('font-family').value;
+    ctx.font = `${fontSize}px ${fontFamily}`;
+    ctx.fillStyle = document.getElementById('text-color').value;
+    ctx.textAlign = 'center';
+    ctx.fillText(textInput.value, canvas.width / 2, canvas.height / 1.8);
+
+    updateCurrentFontSize();
+  }
+
+  function downloadImage() {
+    const randomCode = Math.floor(Math.random() * 100).toString().padStart(2, '0');
+    canvas.toBlob((blob) => {
+      saveAs(blob, `edited_image_${randomCode}.png`);
+    });
+  }
+
+  function updateCurrentFontSize() {
+    const currentFontSizeElement = document.getElementById('current-font-size');
+    currentFontSizeElement.textContent = fontSize;
+  }
+
+  function hideOnDesktop() {
+    const browserWarning = document.getElementById('browser-warning');
+    const isDesktop = window.innerWidth >= 768;
+    if (isDesktop) {
+      browserWarning.style.display = 'none';
+    } else {
+      browserWarning.style.display = 'block';
+    }
+  }
+
+  function preventZoomAndScroll(event) {
+    event.preventDefault();
+  }
+
+  window.addEventListener('resize', hideOnDesktop);
+  window.addEventListener('load', hideOnDesktop);
+});
